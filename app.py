112b40e6a0134f85a488feab437536201eab0ba6
Ozair-Rahman <ozairrahman2003@gmail.com>
2025-10-09 13:45:01 -0400

    Added components to be used in app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+Components to Be Used
+
+Authenticator: https://github.com/mkhorasani/Streamlit-Authenticator
+CSV: Aggregator: https://github.com/PablocFonseca/streamlit-aggrid
+Options Menu: https://github.com/victoryhb/streamlit-option-menu
+Navgation Pane:https://github.com/bvenkatesh-ai/streamlit_pages 
+Keyup: https://github.com/blackary/streamlit-keyup
+
+"""
